0522619476e1e0727961263b088e70cf4431c6f2
Andreas Lind <andreas@one.com>
2015-02-10 11:20:46 +0100

    Test a bit more.

--- test/unexpectedHttp.js
+++ test/unexpectedHttp.js
@@ -1,13 +1,14 @@
 /*global describe, it*/
 var unexpected = require('unexpected'),
-    unexpectedHttp = require('../lib/unexpectedHttp');
+    unexpectedHttp = require('../lib/unexpectedHttp'),
+    http = require('http');
 
 describe('unexpected-http', function () {
     var expect = unexpected.clone().installPlugin(unexpectedHttp);
 
     expect.output.installPlugin(require('magicpen-prism'));
 
-    it('should do a basic request', function (done) {
+    it('should do a basic request @integration', function (done) {
         expect('GET http://www.gofish.dk/', 'to yield response', {
             headers: {
                 'Content-Type': /text\/html/
@@ -15,7 +16,7 @@ describe('unexpected-http', function () {
         }, done);
     });
 
-    it('should fail with a diff', function (done) {
+    it('should fail with a diff @integration', function (done) {
         expect('GET http://www.gofish.dk/', 'to yield response', {
             headers: {
                 'Content-Type': /text\/plain/
@@ -27,7 +28,7 @@ describe('unexpected-http', function () {
         });
     });
 
-    it('should expect an error if the response is given as an error', function (done) {
+    it('should expect an error if the response is given as an error @integration', function (done) {
         var expectedError = new Error('getaddrinfo ENOTFOUND');
         expectedError.code = expectedError.errno = 'ENOTFOUND';
         expectedError.syscall = 'getaddrinfo';
@@ -37,5 +38,64 @@ describe('unexpected-http', function () {
             expectedError,
             done
         );
+    });
+
+    describe('with a local test server', function () {
+        var handleRequest,
+            serverAddress,
+            serverUrl;
+        beforeEach(function () {
+            handleRequest = undefined;
+            var server = http.createServer(function (req, res, next) {
+                res.sendDate = false;
+                handleRequest(req, res, next);
+            }).listen(0);
+            serverAddress = server.address();
+            serverUrl = 'http://' + serverAddress.address + ':' + serverAddress.port + '/';
+        });
+
+        describe('with a JSON response', function () {
+            it('should succeed', function (done) {
+                handleRequest = function (req, res, next) {
+                    res.setHeader('Content-Type', 'application/json');
+                    res.end('{"foo": 123}');
+                };
+                expect('GET ' + serverUrl, 'to yield response', {
+                    body: {
+                        foo: 123
+                    }
+                }, done);
+            });
+
+            it('should fail with a diff', function (done) {
+                handleRequest = function (req, res, next) {
+                    res.setHeader('Content-Type', 'application/json');
+                    res.end('{"foo": 123}');
+                };
+                expect('GET ' + serverUrl, 'to yield response', {
+                    body: {
+                        foo: 456
+                    }
+                }, function (err) {
+                    expect(err, 'to be an', Error);
+                    expect(err.output.toString(), 'to equal',
+                        "expected 'GET " + serverUrl + "' to yield response { body: { foo: 456 } }\n" +
+                        '\n' +
+                        'GET / HTTP/1.1\n' +
+                        'Host: ' + serverAddress.address + ':' + serverAddress.port + '\n' +
+                        '\n' +
+                        'HTTP/1.1 200 OK\n' +
+                        'Content-Type: application/json\n' +
+                        'Connection: keep-alive\n' +
+                        'Transfer-Encoding: chunked\n' +
+                        '\n' +
+                        '{\n' +
+                        '  foo: 123 // should equal 456\n' +
+                        '}'
+                    );
+                    done();
+                });
+            });
+        });
     });
 });
