dd6c90b8943039d1d691d0de7966084c5e22e07d
Andreas Lind <andreaslindpetersen@gmail.com>
2019-01-04 20:42:15 +0100

    eslint --fix .

--- test/browser/unexpectedHttp.js
+++ test/browser/unexpectedHttp.js
@@ -1,21 +1,21 @@
-describe('unexpectedHttp in the browser', function () {
-    var expect = window.weknowhow.expect.clone();
-    expect.installPlugin(window.unexpectedHttp);
-    expect.installPlugin(window.magicPenPrism);
+describe('unexpectedHttp in the browser', function() {
+  var expect = window.weknowhow.expect.clone();
+  expect.installPlugin(window.unexpectedHttp);
+  expect.installPlugin(window.magicPenPrism);
 
-    it('should assert a content type for HTML', function () {
-        return expect('GET /base/test/browser/index.html', 'to yield response', {
-            headers: {
-                'Content-Type': /^text\/html/
-            }
-        });
+  it('should assert a content type for HTML', function() {
+    return expect('GET /base/test/browser/index.html', 'to yield response', {
+      headers: {
+        'Content-Type': /^text\/html/
+      }
     });
+  });
 
-    it('should assert a content type for an image', function () {
-        return expect('GET /base/test/browser/image.png', 'to yield response', {
-            headers: {
-                'Content-Type': /^image\/png/
-            }
-        });
+  it('should assert a content type for an image', function() {
+    return expect('GET /base/test/browser/image.png', 'to yield response', {
+      headers: {
+        'Content-Type': /^image\/png/
+      }
     });
+  });
 });
